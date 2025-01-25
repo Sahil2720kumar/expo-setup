09d56f5d28be82eed6506a200950cc3e4cf41c6c
sahil sah <sahil2720kumar@gmail.com>
2025-01-25 09:31:20 +0530

    slove typescript issue

--- backend/src/db/usersSchema.ts
+++ backend/src/db/usersSchema.ts
@@ -1,3 +1,4 @@
+import { sql } from "drizzle-orm";
 import {
   pgTable,
   serial,
@@ -18,10 +19,10 @@ export const users = pgTable("users", {
   name: varchar("name", { length: 255 }).notNull(), // Full name of the user
   email: varchar("email", { length: 255 }).notNull(), // Email address
   password: varchar("password", { length: 255 }).notNull(), // Hashed password
-  profileImg: varchar("profile_img", { length: 500 }).default(null), // Optional profile image URL
-  bio: text("bio").default(null), // Optional short bio
+  profileImg: varchar("profile_img", { length: 500 }).default(sql`null`), // Optional profile image URL
+  bio: text("bio").default(sql`null`), // Optional short bio
   role: varchar('role', { length: 50 }).default('customer').notNull(), // Role: customer, admin, etc.
-  phone: varchar("phone", { length: 15 }).default(null), // Optional phone number
+  phone: varchar("phone", { length: 15 }).default(sql`null`), // Optional phone number
   isActive: boolean("is_active").default(true).notNull(), // Account status
   addresses: json("addresses").default(null), // JSON array for addresses
   createdAt: timestamp("created_at").defaultNow().notNull(), // Account creation timestamp
