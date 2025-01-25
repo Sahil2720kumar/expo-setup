d51ecde40b00e29869d15334d90c2560195ab920
sahil sah <sahil2720kumar@gmail.com>
2025-01-25 10:49:40 +0530

    slove pg not found error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,29 @@
-import express, { json, urlencoded } from "express"
-import productsRoutes from "./routes/products/index.js"
-import authRoutes from "./routes/auth/index.js"
+import express, { json, urlencoded } from "express";
+import productsRoutes from "./routes/products/index.js";
+import authRoutes from "./routes/auth/index.js";
+import serverless from "serverless-http";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 // Middleware to parse incoming JSON requests
-app.use(json())
-app.use(urlencoded({extended:false}))
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 //Products api
-app.use("/products",productsRoutes)
+app.use("/products", productsRoutes);
 //Auth apis
-app.use("/auth",authRoutes)
+app.use("/auth", authRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World! 766");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World! 766')
-})
+// You don't need to listen to the port when using serverless functions in production
+// if (process.env.NODE_ENV === "dev") {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+// }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-   
+// export const handler = serverless(app);
